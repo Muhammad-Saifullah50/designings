b289b7cc70e16305f5612a1edcf7f2a5432e4e40
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2024-02-11 15:07:51 +0500

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import Navbar from "@/components/Navbar";
 
 export default function HomePage() {
   return (
-    <div>
-      <Navbar/>
-      <Live />
-    </div>
+    <main className="h-screen overflow-hidden">
+      <Navbar />
+      <section className="flex h-full flex-row">
+        <Live />
+
+      </section>
+    </main>
   );
 }
